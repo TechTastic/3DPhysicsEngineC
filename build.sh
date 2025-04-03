d5b09b2419af7e93322547e11c9d8f141a035b05
TechTastic <thetechnomancer123@gmail.com>
2025-04-03 06:31:53 -0500

    It now builds, no physics yet

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p "build"
+gcc src/main.c -o build/main.exe
---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Simulation
+
+// Collision Detection
+
+// Collision Response
+
+int main() {
+    printf("Hello World!\n");
+
+    return 0;
+}
